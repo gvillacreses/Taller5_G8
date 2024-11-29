c52e959043e9ce91aa484ec73a5dc08cdcc33952
jorssanc <jorssanc@espol.edu.ec>
2024-11-28 22:50:14 -0500

    Cambios varios

---
+++ Taller5_G8/src/main/java/com/mycompany/taller5_g8/TareaCompleja.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+
+public class TareaCompleja implements Tarea {
+    private LocalDate fechaVence;
+    private int prioridad;
+    private String proyecto;
+    public TareaCompleja(){
+
+    }
+}
